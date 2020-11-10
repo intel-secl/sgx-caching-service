65be4ce3f9f6556bb29f0f08e82dd1c6ffaa3abc
Raviraj Bhat <raviraj.bhat@intel.com>
2020-11-09 21:16:39 -0800

    ISECL-12419: SKC -Update the PCK CRL handling code to use DER format instead of PEM format

--- constants/constants.go
+++ constants/constants.go
@@ -41,6 +41,7 @@ const (
 	PceId_Key                      = "pce_id"
 	QeId_Key                       = "qe_id"
 	Ca_Key                         = "ca"
+	Encoding_Value                 = "der"
 	Type_Key                       = "type"
 	Ca_Processor                   = "processor"
 	Fmspc_Key                      = "fmspc"
