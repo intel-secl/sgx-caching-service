2ee99379954a64868a29df9f50750297c403ccd3
Rajashekara, Rashmi <rashmi.rajashekara@intel.com>
2021-04-18 19:41:46 -0700

    ISECL-14226: removed sensitive data

--- dist/image/entrypoint.sh
+++ dist/image/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+source /etc/secret-volume/secrets.txt
+export SCS_ADMIN_USERNAME
+export SCS_ADMIN_PASSWORD
+export SCS_DB_USERNAME
+export SCS_DB_PASSWORD
+export BEARER_TOKEN
+
 USER_ID=$(id -u)
 LOG_PATH=/var/log/scs
 CONFIG_PATH=/etc/scs
